12b66cefa56f3a9ade7a98af46d8447e8257641d
rbyers87 <35844687+rbyers87@users.noreply.github.com>
2025-08-21 16:21:21 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,11 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
- // base: "/FleetApp2",
+  // base: "/FleetApp2",
   optimizeDeps: {
     exclude: ['lucide-react'],
+  },
+  server: {
+    historyApiFallback: true,
   },
 });
